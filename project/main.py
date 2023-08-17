2140fe5c09a45da1101366f3d927028f3f83f11c
Christian Rodriguez <christianrodriguez@ChristiansAir2.lan>
2023-08-17 01:08:06 -0600

    Added Dice Rule

--- project/main.py
+++ project/main.py
@@ -3,7 +3,7 @@ import random
 class MonopolyBoardAssembler:
     def __init__(self):
         self.board = [[None]] * 11
-        self.players = []
+        self.players = {}
         self.bank_cash = 20580
         self.tokens = ['battleship', 
                       'boot', 'cannon', 
@@ -23,37 +23,59 @@ class MonopolyBoardAssembler:
                 if i == 0 :
                     self.board.append(0)
 
-  
-
-    def throw_dice(self):
+    def player_throw_dice(name, self):
         dice_one = random.randrange(1, 7)
         dice_two = random.randrange(1, 7)
-        print(dice_one, dice_two)
-        return dice_one
 
-    def create_players(self,name, token):
-        self.players.append({
+        if dice_one == dice_two :
+            self.players[name][num_of_roles] += 1
+
+            if self.players[name][num_of_roles] == 1:
+                return dice_one + dice_two
+            
+            if self.players[name][num_of_roles] == 2:
+                return dice_one + dice_two
+            
+            if self.players[name][num_of_roles] == 3:
+                a = "jail"
+                # Need to come back and put in jail
+            
+        return dice_one + dice_two
+    
+    def create_players(self, name, token):
+        self.players[name] = {
             "name" : name,
             "token" : self.get_token(token),
             "cash" : self.bank_withdraw(1500),
-            "properties" : []
-        })
+            "properties" : [],
+            "num_of_roles" : 0
+        }
 
-    def get_token(self,token):
+        print(self.players)
+        print(self.tokens)
+
+    def get_token(self, token):
         "Check if player token choice is valid"
+        
         if token.lower() in self.tokens:
             self.tokens.remove(token)
             return token
-        print(f'Invalid, choose from list: {self.tokens}')
+        
+        user_token = input(f'Invalid Token. Avaliable Tokens: {self.tokens}\n')
+        return self.get_token(user_token)        
+        
+        
 
-        token = input('What will you choose?\n')
-        return self.get_token(token)
 
-    def bank_withdraw(self,amount):
+    def bank_withdraw(self, amount):
         new_bank_cash = self.bank_cash - amount
         return amount
 
-MonopolyBoardAssembler.create_players('Christian', 'boot')
+    
+
+# https://www.hasbro.com/common/instruct/00009.pdf
 
   
-
+a = MonopolyBoardAssembler()
+a.create_players('chris','dog')
+a.create_players('jesss','cat')
